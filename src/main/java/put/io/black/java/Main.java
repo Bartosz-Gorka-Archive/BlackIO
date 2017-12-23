fe57d30ea9ade554393950defbe8b2c4ae514a3d
Bartosz Górka <bartosz.gorka@appunite.com>
2017-12-23 23:03:54 +0100

    Travis

--- src/main/java/put/io/black/java/Main.java
+++ src/main/java/put/io/black/java/Main.java
@@ -1,8 +1,8 @@
-package com.blackio;
+package put.io.black.java;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Word!");
+        System.out.println("Hello BlackIO!");
     }
 }
