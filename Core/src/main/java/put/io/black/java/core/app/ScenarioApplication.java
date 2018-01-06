bf4bef8ab607ed7ab253f686d592e7a5ecdf5bb3
Bartosz Górka <bartosz@kmcstudio.pl>
2018-01-06 12:25:43 +0100

    Split modules in project (#17)
    * Update badges
    * Split projects as modules
    * Travis CI

---
+++ Core/src/main/java/put/io/black/java/core/app/ScenarioApplication.java
@@ -0,0 +1,20 @@
+package put.io.black.java.core.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main Application code to ensure run SpringApplication
+ * @see SpringApplication
+ */
+@SpringBootApplication(scanBasePackages = {"put.io.black.java.core.rest"})
+public class ScenarioApplication {
+
+    /**
+     * Run SpringApplication with our ScenarioApplication class
+     * @param args Program arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(ScenarioApplication.class, args);
+    }
+}
